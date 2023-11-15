ca925612c5fcf919ba2f0c844339e65493338030
SANAYEE <dudtkd0219@gmail.com>
2023-11-15 14:01:00 +0900

    login and logout success

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import News from './News';
 import './App.css'
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography, } from '@mui/material';
+import {call, signin, signout} from './service/ApiService'
+import Login from './Login';
 
 function App() {
 
+  let navigationBar = (
+    <AppBar position='static'>
+      <Toolbar>
+        <Grid justifyContent='space-between' container>
+          <Grid item>
+            <Typography variant='h6'>news list</Typography>
+          </Grid>
+        </Grid>
+        <Grid item>
+          {localStorage.getItem("ACCESS_TOKEN") ? <Button color='inherit' onClick={signout}>LogOut</Button> : <a style={{color: 'white'}} href='/login'>Login</a> }
+          
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
   return (
     <>
-      <News />
+      {navigationBar}
+      <Container maxWidth="md">
+        <News />
+      </Container>
     </>
   )
 }
